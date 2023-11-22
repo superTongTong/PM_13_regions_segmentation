90d978f2eb8fba8b2c8859fc459a541a64c63dea
YaoTong <y.tong@student.tue.nl>
2023-11-22 13:10:14 +0100

    add command for move plan between datasets.

--- Data_conversion/nnunet_plan_preprocessing.py
+++ Data_conversion/nnunet_plan_preprocessing.py
@@ -7,8 +7,10 @@ def plan_and_preprocess():
     setup_nnunet()
 
     # set the command-line arguments as needed.
-    command = "nnUNetv2_plan_and_preprocess -d 2 -pl ExperimentPlanner -c 3d_fullres -np 2 --verify_dataset_integrity"
-
+    # command = "nnUNetv2_plan_and_preprocess -d 1 -pl ExperimentPlanner -c 3d_fullres -np 2 --verify_dataset_integrity"
+    # command = ("nnUNetv2_move_plans_between_datasets -s 4 -t 2 "
+    #            "-sp plans -tp nnUNetPlans_pretrain")
+    command = "nnUNetv2_preprocess -d 2 -plans_name nnUNetPlans_pretrain"
     # Run the command
     subprocess.run(command, shell=True)
 
