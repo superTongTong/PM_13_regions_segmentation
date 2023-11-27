3a008a50dfaca090002df038a3f46a5a4c873f0c
YaoTong <y.tong@student.tue.nl>
2023-11-27 16:33:36 +0100

    update

--- Data_conversion/nnunet_plan_preprocessing.py
+++ Data_conversion/nnunet_plan_preprocessing.py
@@ -8,9 +8,9 @@ def plan_and_preprocess():
 
     # set the command-line arguments as needed.
     # command = "nnUNetv2_plan_and_preprocess -d 1 -pl ExperimentPlanner -c 3d_fullres -np 2 --verify_dataset_integrity"
-    # command = ("nnUNetv2_move_plans_between_datasets -s 4 -t 2 "
+    # command = ("nnUNetv2_move_plans_between_datasets -s 4 -t 3 "
     #            "-sp plans -tp nnUNetPlans_pretrain")
-    command = "nnUNetv2_preprocess -d 2 -plans_name nnUNetPlans_pretrain"
+    command = "nnUNetv2_preprocess -d 3 -plans_name nnUNetPlans_pretrain -c 3d_fullres -np 2 --verify_dataset_integrity"
     # Run the command
     subprocess.run(command, shell=True)
 
