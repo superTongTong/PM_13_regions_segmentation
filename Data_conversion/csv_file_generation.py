cdcf00c5a235c386b5d513b9aa067f5c5c4f77b3
YaoTong <y.tong@student.tue.nl>
2023-11-27 11:45:34 +0100

    add code for test data set

--- Data_conversion/csv_file_generation.py
+++ Data_conversion/csv_file_generation.py
@@ -3,21 +3,27 @@ import csv
 import random
 
 # Specify the directory where you want to search for files
-targeted_folder = "C:/Users/20202119/PycharmProjects/segmentation_PM/data/abdomenCT_60_test_cases/images"
+targeted_folder = "D:/master/graduation_project/data_set/abdomenCT_198_test_cases/images"
 
 # Initialize a list to store file names and splits
 data = []
-
+train_ratio, val_ratio, test_ratio = 0.6, 0.2, 0.2 # for 198 cases, 117 train, 42 val, 39 test
 # Iterate through the files in the directory
 for root, dirs, files in os.walk(targeted_folder):
     for file in files:
         if file.endswith(".nii.gz"):
-            # Randomly assign "train" or "val" with a 95% / 5% split
-            split = "train" if random.random() < 0.95 else "val"
+            # Randomly assign "train", "val", or "test" with specified ratios
+            random_number = random.random()
+            if random_number < train_ratio:
+                split = "train"
+            elif random_number < train_ratio + val_ratio:
+                split = "val"
+            else:
+                split = "test"
             data.append({"image_id": file, "split": split})
 
 # Specify the output CSV file
-csv_file = "../data/abdomenCT_60_test_cases/meta.csv"
+csv_file = "D:/master/graduation_project/data_set/abdomenCT_198_test_cases/meta.csv"
 
 # Write the data to the CSV file
 with open(csv_file, "w", newline="") as csv_file:
