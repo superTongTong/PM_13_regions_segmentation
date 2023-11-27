2cdd28e305f965dfdf23fd58b3263fa560707ea0
YaoTong <y.tong@student.tue.nl>
2023-11-27 16:36:44 +0100

    fine-tune 10 epochs with new abdomenCT data.

--- train.py
+++ train.py
@@ -3,13 +3,12 @@ import subprocess
 from pathlib import Path
 
 
-
 def train():
     # setup the nnUNet environment variables
     setup_nnunet()
     pre_trained_weight = Path("./data/nnunet/results/Dataset291_TotalSegmentator_part1_organs_1559subj/nnUNetTrainerNoMirroring__nnUNetPlans__3d_fullres/fold_0/checkpoint_final.pth")
     # set the command-line arguments as needed.
-    command = f"nnUNetv2_train 2 3d_fullres 0 -tr nnUNetTrainer_5epochs -p nnUNetPlans_pretrain -pretrained_weights {pre_trained_weight}"
+    command = f"nnUNetv2_train 3 3d_fullres 0 -tr nnUNetTrainer_10epochs -p nnUNetPlans_pretrain -pretrained_weights {pre_trained_weight}"
     # command = f"nnUNetv2_train 2 3d_fullres 0 -tr nnUNetTrainer_5epochs"
     # Run the command
     subprocess.run(command, shell=True)
