de49ae4d507e6970569089ac2928c466cdcc662d
YaoTong <y.tong@student.tue.nl>
2024-01-19 10:40:55 +0100

    update

--- Data_conversion/nnunet_plan_preprocessing.py
+++ Data_conversion/nnunet_plan_preprocessing.py
@@ -10,7 +10,8 @@ def plan_and_preprocess():
     # command = "nnUNetv2_plan_and_preprocess -d 1 -pl ExperimentPlanner -c 3d_fullres -np 2 --verify_dataset_integrity"
     # command = ("nnUNetv2_move_plans_between_datasets -s 4 -t 3 "
     #            "-sp plans -tp nnUNetPlans_pretrain")
-    command = "nnUNetv2_preprocess -d 3 -plans_name nnUNetPlans_pretrain -c 3d_fullres -np 2 --verify_dataset_integrity"
+    command = ("nnUNetv2_extract_fingerprint -d 3")
+    # command = "nnUNetv2_preprocess -d 3 -plans_name nnUNetPlans_pretrain -c 3d_fullres -np 2 --verify_dataset_integrity"
     # Run the command
     subprocess.run(command, shell=True)
 
