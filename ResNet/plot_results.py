f00895a674a40ec79abe3ad39252cb7384744ec9
YaoTong <y.tong@student.tue.nl>
2024-04-23 10:53:30 +0200

    add code for plot confusion matrix.

--- ResNet/plot_results.py
+++ ResNet/plot_results.py
@@ -1,6 +1,39 @@
 import matplotlib.pyplot as plt
 
 
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues, save_path=None):
+    """
+    This function prints and plots the confusion matrix.
+    """
+    import numpy as np
+    import itertools
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment='center',
+                 color='white' if cm[i, j] > thresh else 'black')
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    if save_path:
+        plt.savefig(f'{save_path}confusion_matrix.png')
+    else:
+        plt.show()
+
 def plot_metrics(train_loss, val_loss, accuracy, save_path=None):
     epochs = range(1, len(train_loss) + 1)
 
