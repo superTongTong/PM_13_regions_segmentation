59ca7f8220f0df9ec14283cca5c58443f9da0069
YaoTong <y.tong@student.tue.nl>
2024-04-25 17:01:16 +0200

    update code.

--- ResNet/MedicalNet/MedicalNet.py
+++ ResNet/MedicalNet/MedicalNet.py
@@ -25,6 +25,6 @@ class MedicalNet(nn.Module):
 
     def forward(self, x):
         features = self.model(x)
+        return torch.softmax(self.fc(features), dim=1)
         # return torch.sigmoid_(self.fc(features))
-        return nn.Softmax(self.fc(features))
 
